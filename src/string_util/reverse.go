42855d43ff91b6ab6651a6cd6d3625f499fb532a
darvid7 <bvdarvid@gmail.com>
2017-03-01 02:25:03 +1100

    fix typo, remove swp file

--- src/string_util/reverse.go
+++ src/string_util/reverse.go
@@ -5,7 +5,7 @@ package string_util
 // Reverse returns a string reversed left to right.
 func Reverse(s string) string {
 	r := []rune(s) // Array of runes from string s.
-	// A run in an integer value mapped to unicode value i.e. 97 = a
+	// A rune in an integer value mapped to unicode value i.e. 97 = a
 	// for i = 0, j = len(r) -1
 	// 	while i < len(r)/2 # Go half way
 	//		i += 1, j -= 1
